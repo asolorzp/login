8f06af6d92c31d63a1673169fa80a234e16f985d
Abraham Solorzano <abraham.solorzano@pjchile.com>
2020-09-03 12:42:39 -0400

    add

--- handler.js
+++ handler.js
@@ -7,7 +7,7 @@ module.exports.helloWorld = (event, context, callback) => {
       'Access-Control-Allow-Origin': '*', // Required for CORS support to work
     },
     body: JSON.stringify({
-      message: 'Test'
+      message: 'Test new'
     }),
   };
 
